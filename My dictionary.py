1af839ea5606b6d19f0484b8704194e9c6a3388d
Godfrey-Vila <vila.godfreyc@gmail.com>
2022-11-12 22:41:21 +0800

    editing of Option 1

--- My dictionary.py
+++ My dictionary.py
@@ -4,6 +4,7 @@ print("******** SECTION BSCOE 2-2 ***********")
 print("******* Sir Danilo Madrigalejos ******")
 print("")
 
+contacts={}
 Loop = True
 while Loop:
     print("********** MENU **********")
@@ -11,20 +12,21 @@ while Loop:
     print("2. search ")
     print("3. exit ")
     print("**************************")
-    Option = int(input("please choose from the menu: "))
-    if Option == 1:
-        First_Name = str(input("please state your fist name: ").upper())
-        Middle_Initial = str(input("please state your middle initial: ").upper())
-        Last_Name = str(input("please state your last name: ").upper())
-        Age= int(input("How old are you: "))
-        Address = input("Tell me your address: ")
-        Phone = int(input("please indicate your phone number: #"))
-        print("Name: ", First_Name, Middle_Initial, Last_Name)
-        print("Age: ", Age)
-        print("Address: ", Address)
-        print("Phone number: ", Phone)
+    print("")
 
-    Loop = False
+    Option = int(input("please choose from the menu: "))
+    print("")
+    if Option == 1:
+        Name= str(input("Please enter your name (example: Godfrey C. Vila) : ").capitalize())
+        Age= int(input("How old are you: "))
+        Address = input("Tell me your address: ").capitalize()
+        Phone = int(input("please indicate your phone number: #"))
+        contacts = {Name: {"Age": Age, "Address": Address, "Phone": Phone}}
+        print("Saved")
+        print(contacts)
+    elif Option == 3:
+        print("Exit")
+        Loop = False
 
 
 
---
+++ practice.py
@@ -0,0 +1,14 @@
+Name={
+    'Martin': 20,
+    'Luke': 10
+      }
+Name1={
+    'Martinon': 20,
+    'Luke graham': 20
+}
+Name.update(Name1)
+print(Name)
+
+Name={
+    Martin:{20:'blk 1', }
+}
