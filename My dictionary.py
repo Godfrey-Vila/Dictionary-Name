d809747361b7c828cc47ddbf482f213800b1d1d4
Godfrey-Vila <vila.godfreyc@gmail.com>
2022-11-12 19:47:16 +0800

    option 1

--- My dictionary.py
+++ My dictionary.py
@@ -11,6 +11,19 @@ while Loop:
     print("2. search ")
     print("3. exit ")
     print("**************************")
+    Option = int(input("please choose from the menu: "))
+    if Option == 1:
+        First_Name = str(input("please state your fist name: ").upper())
+        Middle_Initial = str(input("please state your middle initial: ").upper())
+        Last_Name = str(input("please state your last name: ").upper())
+        Age= int(input("How old are you: "))
+        Address = input("Tell me your address: ")
+        Phone = int(input("please indicate your phone number: #"))
+        print("Name: ", First_Name, Middle_Initial, Last_Name)
+        print("Age: ", Age)
+        print("Address: ", Address)
+        print("Phone number: ", Phone)
+
     Loop = False
 
 
