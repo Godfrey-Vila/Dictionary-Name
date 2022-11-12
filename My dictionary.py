c8be168564bcaac59e510e4f3500773430cf4191
Godfrey-Vila <vila.godfreyc@gmail.com>
2022-11-12 19:27:27 +0800

    adding my menu

--- My dictionary.py
+++ My dictionary.py
@@ -3,3 +3,15 @@ print("********** GODFREY VILA **************")
 print("******** SECTION BSCOE 2-2 ***********")
 print("******* Sir Danilo Madrigalejos ******")
 print("")
+
+Loop = True
+while Loop:
+    print("********** MENU **********")
+    print("1. add an item ")
+    print("2. search ")
+    print("3. exit ")
+    print("**************************")
+    Loop = False
+
+
+
